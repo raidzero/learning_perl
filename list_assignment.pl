e73a5f93578084d315a8f610893e59883e3fcd30
raidzero <paul.raidzero@gmail.com>
2012-07-23 12:49:34 -0600

    list assignmest example

---
+++ list_assignment.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+($crono, $marle, $lucca, $magus) = ("lightning", "water", "fire", "shadow");
+
+printf("Crono: %s\nMarle: %s\nLucca: %s\nMagus: %s\n", $crono, $marle, $lucca, $magus);
